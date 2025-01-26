5387be8735ba6af60f13efa73a07c7575fdf72d1
umarxdd <mihr.e.kahyan@gmail.com>
2025-01-26 08:45:57 -0800

    embedded video

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,13 +3,23 @@ import { GoPlay } from "react-icons/go";
 
 const Carousel = () => {
   const content = [
-    { img: "/images/jakevpaul.png", src: "https://vimeo.com/1025365469" },
-    { img: "/images/jakevpaul.png", src: "https://vimeo.com/1023150048" },
+    {
+      img: "/images/jakevpaul.png",
+      src: "https://player.vimeo.com/video/1025365469",
+    },
+    {
+      img: "/images/china.png",
+      src: "https://player.vimeo.com/video/1023150048",
+    },
 
-    { img: "/images/proven.png", src: "https://vimeo.com/1023151062" },
+    {
+      img: "/images/proven.png",
+      src: "https://player.vimeo.com/video/1023151062",
+    },
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleNext = () => {
     setCurrentIndex((prev) => (prev == content.length - 1 ? 0 : prev + 1));
@@ -18,16 +28,22 @@ const Carousel = () => {
   const handlePrev = () => {
     setCurrentIndex((prev) => (prev == 0 ? content.length - 1 : prev - 1));
   };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   return (
     <>
       <div className="relative h-[35rem] overflow-hidden w-full">
         <div className="absolute inset-0">
-          <a href={content[currentIndex].src} target="_blank">
-            <img
-              src={content[currentIndex].img}
-              className="object-cover w-full h-full cursor-pointer transition-transform duration-500"
-            />
-          </a>
+          <img
+            src={content[currentIndex].img}
+            className="object-cover w-full h-full cursor-pointer transition-transform duration-500"
+          />
         </div>
 
         <button
@@ -57,11 +73,35 @@ const Carousel = () => {
         </div>
 
         <div className="absolute text-9xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white hover:opacity-75">
-          <a href={content[currentIndex].src} target="_blank">
-            <GoPlay />
-          </a>
+          <GoPlay onClick={openModal} />
         </div>
       </div>
+      {isModalOpen && (
+        <>
+          <div className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-50">
+            <div className="">
+              <button
+                className="absolute top-3 right-3 text-gray-200 hover:text-gray-400 text-2xl"
+                onClick={closeModal}
+              >
+                ✕
+              </button>
+            </div>
+
+            <div className="aspect-video w-[80%]">
+              <iframe
+                src={content[currentIndex].src}
+                width="100%"
+                height="100%"
+                allow="autoplay; fullscreen; picture-in-picture"
+                allowFullScreen
+                className="rounded-lg w-full h-full"
+                style={{ border: "none" }}
+              ></iframe>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
